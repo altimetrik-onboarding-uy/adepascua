562b77d667bab6d991a587acd4198159bb392114
adepascua <adepascua@altimetrik.com>
2019-01-15 15:08:07 -0300

    Commit part 3

--- src/aura/BloggistEditTab/BloggistEditTabController.js
+++ src/aura/BloggistEditTab/BloggistEditTabController.js
@@ -1,20 +1,18 @@
-({      /*  
-    doInit : function(component, event, helper) {
-		helper.autoSave(component, event, helper);
-	}, */   
+({
+
     recordUpdated: function (component, event, helper) {
         var changeType = event.getParams().changeType;
         if (changeType === "ERROR") { /* handle error; do this first! */ }
-        else if (changeType === "LOADED") {  
+        else if (changeType === "LOADED") {
+            var loadedText = component.get("v.simpleRecord.Content__c")
+            component.set("v.updatedText", loadedText);
             var input = component.find("input").get("v.value");
-            helper.convertToMarkdown(input); 
+            helper.convertToMarkdown(input);
         }
-            else if (changeType === "REMOVED") { /* handle record removal */ }
-                else if (changeType === "CHANGED") {
-                    /* handle record change; reloadRecord will cause you to lose your current record, including any changes you’ve made */           
-                    component.find("recordData").reloadRecord();
-                }
-    },	
+        else if (changeType === "REMOVED") { /* handle record removal */ }
+        else if (changeType === "CHANGED") {
+        }
+    },
     handleSelect: function (component, event, helper) {
         var stepName = event.getParam("detail").value;
         var toastEvent = $A.get("e.force:showToast");
@@ -24,12 +22,12 @@
         });
         toastEvent.fire();
     },
-    
-    submitForApproval: function (component, event, helper) {  
-        if (component.get("v.simpleRecord.Status__c") == "Draft"){            
+
+    submitForApproval: function (component, event, helper) {
+        if (component.get("v.simpleRecord.Status__c") == "Draft") {
             component.set("v.simpleRecord.Status__c", "Under Review");
-            component.set("v.simpleRecord.Submit_for_Approval__c", true)            
-            component.find("recordData").saveRecord(function(saveResult) {
+            component.set("v.simpleRecord.Submit_for_Approval__c", true)
+            component.find("recordData").saveRecord(function (saveResult) {
                 if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
                     // record is saved successfully
                     var resultsToast = $A.get("e.force:showToast");
@@ -43,31 +41,46 @@
                     console.log("User is offline, device doesn't support drafts.");
                 } else if (saveResult.state === "ERROR") {
                     // handle the error state
-                    console.log('Problem saving post, error: ' + 
-                                JSON.stringify(saveResult.error));
+                    console.log('Problem saving post, error: ' +
+                        JSON.stringify(saveResult.error));
                 } else {
                     console.log('Unknown problem, state: ' + saveResult.state +
-                                ', error: ' + JSON.stringify(saveResult.error));
+                        ', error: ' + JSON.stringify(saveResult.error));
                 }
             });
         }
-        else
-        {
+        else {
             var resultsToast = $A.get("e.force:showToast");
             resultsToast.setParams({
                 "title": "Notice:",
                 "message": "This post has already submitted for review."
             });
             resultsToast.fire();
-        }   
-    },    
-    textUpdated : function(component, event, helper) {
+        }
+    },
+    textUpdated: function (component, event, helper) {
         var input = component.find("input").get("v.value");
-        helper.convertToMarkdown(input);   
-       
-        window.setTimeout($A.getCallback(function() {
+        helper.convertToMarkdown(input);
+        window.setTimeout($A.getCallback(function () {
             helper.autoSave(component, event, helper);
         }), 3000
-                         );
+        );
     },
+    markdownInputOnFocus: function (component, event, helper) {
+        var ctrlhold = component.get("v.isCtrlHold");
+        var shifthold = component.get("v.isShiftHold");
+        document.onkeyup = function (e) {
+            if (e.which == 17) //17 is the code of Ctrl button
+                component.set("v.isCtrlHold", false);
+            if (e.which == 16) //16 is the code of Shift button
+                component.set("v.isShiftHold", false);
+        }
+        document.onkeydown = function (e) {
+            if (e.which == 17)
+                component.set("v.isCtrlHold", true);
+            if (e.which == 16)
+                component.set("v.isShiftHold", true);
+            helper.shorcutManager(component, e);
+        }
+    }
 })
--- src/aura/BloggistEditTab/BloggistEditTabHelper.js
+++ src/aura/BloggistEditTab/BloggistEditTabHelper.js
@@ -1,75 +1,101 @@
 ({
-    convertToMarkdown : function(text) {
-        
-        console.log("helper-markdown");        
-        
+    convertToMarkdown: function (text, component) {
         // Set options
-		/*
         marked.setOptions({
-            renderer: new marked.Renderer(),            
-            pedantic: false,
-            gfm: true,
-            tables: true,
-            breaks: false,
-            sanitize: false,
-            smartLists: true,
-            smartypants: false,
-            xhtml: false
+            "baseUrl": null,
+            "breaks": false,
+            "gfm": true,
+            "headerIds": true,
+            "headerPrefix": "",
+            "highlight": null,
+            "langPrefix": "language-",
+            "mangle": true,
+            "pedantic": false,
+            "sanitize": false,
+            "sanitizer": null,
+            "silent": false,
+            "smartLists": false,
+            "smartypants": false,
+            "tables": true,
+            "xhtml": false
         });
-        */
-         marked.setOptions({
-         "baseUrl": null,
-         "breaks": false,
-         "gfm": true,
-         "headerIds": true,
-         "headerPrefix": "",
-         "highlight": null,
-         "langPrefix": "language-",
-         "mangle": true,
-         "pedantic": false,
-         "sanitize": false,
-         "sanitizer": null,
-         "silent": false,
-         "smartLists": false,
-         "smartypants": false,
-         "tables": true,
-         "xhtml": false
-        });
-        
-        
-        
+
         // Compile
-        var output = marked(text); 
-        console.log(output);        
+        var output = marked(text);
         var htmlOutputDiv = document.getElementById("output");
         htmlOutputDiv.innerHTML = output;
     },
-    
-    autoSave : function (component, event, helper){        
-    var lastText = document.getElementById("output").innerHTML;
-    component.set("v.simpleRecord.Content__c", lastText);
-	component.find("recordData").saveRecord($A.getCallback(function(saveResult) {
-                if (saveResult.state === "SUCCESS") {
-                    // record is saved successfully
-                    var resultsToast = $A.get("e.force:showToast");
-                    resultsToast.setParams({
-                        "title": "Saved",
-                        "message": "Saved!"
-                    });
-                    resultsToast.fire();
-                } else if (saveResult.state === "INCOMPLETE") {
-                    // handle the incomplete state
-                    //console.log("User is offline, device doesn't support drafts.");
-                } else if (saveResult.state === "ERROR") {
-                    // handle the error state
-                    //console.log('Problem saving post, error: ' + 
-                                 //JSON.stringify(saveResult.error));
-                } else {
-                   // console.log('Unknown problem, state: ' + saveResult.state +
-                               // ', error: ' + JSON.stringify(saveResult.error));
-                }
-            }))
-        
+
+    autoSave: function (component, event, helper) {
+        var lastText = document.getElementById("output").innerHTML;
+        component.set("v.simpleRecord.Content__c", lastText);
+        component.find("recordData").saveRecord($A.getCallback(function (saveResult) {
+            if (saveResult.state === "SUCCESS") {
+                // record is saved successfully
+                var resultsToast = $A.get("e.force:showToast");
+                resultsToast.setParams({
+                    "title": "Saved",
+                    "message": "Saved!",
+                    "type": "success",
+                    "mode": "pester"
+                });
+                resultsToast.fire();
+            } else if (saveResult.state === "INCOMPLETE") {
+                // handle the incomplete state
+                console.log("User is offline, device doesn't support drafts.");
+            } else if (saveResult.state === "ERROR") {
+                // handle the error state
+                console.log('Problem saving post, error: ' + JSON.stringify(saveResult.error));
+            } else {
+                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+            }
+        }))
+    },
+    shorcutManager: function (component, e) {
+
+        var isControlHold = component.get("v.isCtrlHold");
+        var isShiftHold = component.get("v.isShiftHold");
+        var keyPressed = e.which;
+
+        switch (keyPressed) {
+            case 76: //76 is the code of L button
+                if (isControlHold) {
+                    if (isShiftHold) {
+                        e.preventDefault(); //prevent browser from the default behavior
+
+                        //Insert block of code 
+                        var text = component.get("v.updatedText");
+                        text = "```insert_code_here```" + text;
+                        component.set("v.updatedText", text);
+
+                    }
+                    else {
+                        e.preventDefault(); //prevent browser from the default behavior
+
+                        //Insert inline code 
+                        var text = component.get("v.updatedText");
+                        text = "`insert_code_here`" + text;
+                        component.set("v.updatedText", text);
+                    }
+                } break;
+            case 73: //76 is the code of I button
+                if (isControlHold) {
+                    e.preventDefault(); //prevent browser from the default behavior
+
+                    //Render as italic 
+                    var text = component.get("v.updatedText");
+                    text = "*insert_text_here*" + text;
+                    component.set("v.updatedText", text);
+                } break;
+            case 66: //76 is the code of B button
+                if (isControlHold) {
+                    e.preventDefault(); //prevent browser from the default behavior
+
+                    //Render as bold 
+                    var text = component.get("v.updatedText");
+                    text = "**insert_text_here**" + text;
+                    component.set("v.updatedText", text);
+                } break;
+        }
     }
-   
 })
